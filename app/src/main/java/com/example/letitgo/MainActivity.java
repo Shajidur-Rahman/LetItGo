86f68ed644714c582ababed8ff6fc6bc5e9368b5
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-12-03 15:56:41 +0600

    right till now

--- app/src/main/java/com/example/letitgo/MainActivity.java
+++ app/src/main/java/com/example/letitgo/MainActivity.java
@@ -8,12 +8,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.letitgo.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
     }
 
---
+++ app/src/main/java/com/example/letitgo/UsersAdapter.java
@@ -0,0 +1,56 @@
+package com.example.letitgo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.letitgo.databinding.RawConversationBinding;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UsersViewHolder>{
+
+    Context context;
+    ArrayList<User> users;
+
+    public UsersAdapter(Context context, ArrayList<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.raw_conversation, parent, false);
+
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersViewHolder holder, int position) {
+        User user = users.get(position);
+
+        holder.binding.insideName.setText(user.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder{
+
+        RawConversationBinding binding;
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = RawConversationBinding.bind(itemView);
+
+
+
+        }
+    }
+}
