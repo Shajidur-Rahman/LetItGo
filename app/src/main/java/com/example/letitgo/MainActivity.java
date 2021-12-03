6861fa000573cf944840aa9b7769c1a3395bce6e
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-12-03 12:02:06 +0600

    design main activity finished

--- app/src/main/java/com/example/letitgo/MainActivity.java
+++ app/src/main/java/com/example/letitgo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.letitgo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                Toast.makeText(MainActivity.this, "Search clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.invite:
+                Toast.makeText(MainActivity.this, "invite clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.group:
+                Toast.makeText(MainActivity.this, "Search group", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(MainActivity.this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmanu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/example/letitgo/PhoneNumberActivity.java
+++ app/src/main/java/com/example/letitgo/PhoneNumberActivity.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 
 import com.example.letitgo.databinding.ActivityPhoneNumberBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PhoneNumberActivity extends AppCompatActivity {
 
     ActivityPhoneNumberBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null){
+            Intent intent = new Intent(PhoneNumberActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
 
         getSupportActionBar().hide();
         binding.PhoneNumber.requestFocus();
